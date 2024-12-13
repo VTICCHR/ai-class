b0685f407304eb444aa3fef99804c5bf9cb1de7b
bin <113090944+VTICCHR@users.noreply.github.com>
2024-12-13 21:06:40 +0900

    Add files via upload

---
+++ example6.py
@@ -0,0 +1,8 @@
+# example6.py
+# This script demonstrates a simple modulus function
+
+def modulus_numbers(a, b):
+    return a % b
+
+if __name__ == "__main__":
+    print("Remainder:", modulus_numbers(10, 3))
